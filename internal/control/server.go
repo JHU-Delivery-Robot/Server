57dba1ed5fcfbb2504efcda02e61c6969477ccde
Aaren Wong <aarenclwong@gmail.com>
2022-04-02 17:50:35 -0400

    completed docker compose for prototype
    docker-compose build and docker-compose up to rebuild the docker-compose image
    changed to go v1.18
    networking over docker-compose works between osrm and our go server

--- internal/control/server.go
+++ internal/control/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"io/ioutil"
 	"log"
+	"net/http"
 
 	"github.com/JHU-Delivery-Robot/Server/internal/routing"
 	pb "github.com/JHU-Delivery-Robot/Server/spec"
@@ -14,11 +15,19 @@ type Server struct {
 }
 
 func (s *Server) GetRoute(ctx context.Context, in *pb.Coords) (*pb.RoutePoint, error) {
-	log.Printf("Received Coords: (%v, %v)\n", in.GetX(), in.GetY())
+	log.Printf("Received OSRM Coords: (%v, %v)\n", in.GetX(), in.GetY())
 
-	data, err := ioutil.ReadFile("./testRoute.txt")
+	//data, err := ioutil.ReadFile("./testRoute.txt")
+
+	resp, err := http.Get("http://osrm:5000/route/v1/driving/-76.6215169429779,39.32603594879762;-76.62238597869873,39.328870107766434?overview=full&steps=true&geometries=geojson")
 
 	if err == nil {
+		defer resp.Body.Close()
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+
 		var nextRoute routing.OSRMRoute = routing.GetOSRMRoute(data)
 		var points []*pb.Point
 		for i := 0; i < len(nextRoute.Latitude); i++ {
